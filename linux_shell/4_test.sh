a4cf9d645d3af2c03d4c346f9547237ef9a817ce
xuke <iceke@foxmail.com>
2020-03-29 21:12:04 +0800

    4_test: finish 4_test for part_14
    [14][New]
    * add 4_test for 14 capter
    * test ok.

--- linux_shell/4_test.sh
+++ linux_shell/4_test.sh
@@ -39,6 +39,113 @@ params=$#
 echo The last parameter is $params
 echo The last parameter is ${!#}
 
-# 
+# 抓取所有参数，$* 和 $@
+# $* 会将命令行上提供的所有参数当作一个单词保存
+# $@ 会将命令行上提供的所有参数当作同一字符串中的多个独立的单词
+echo
+echo "Using the \$* method: $*"
+echo
+echo "Using the \$@ method: $@"
+# 遍历：
+count=1
+for param in "$*"
+do
+	echo "\$* Parameter #$count = $param"
+	count=$[ $count + 1 ]
+done
+echo "------------------------------"
+count=1
+for param in "$@"
+do
+	echo "\$@ Parameter #$count = $param"
+	count=$[ $count + 1 ]
+done
+
+# 移动变量
+# 使用 shift 命令时,默认情况下它会将每个参数变量向左移动一个位置
+# 而变量 $1 的值则会被删除(注意,变量 $0 的值,即程序名,不会改变)
+count=1
+while [ -n "$1" ]
+do
+	echo "Parameter #$count = $1"
+	count=$[ $count + 1 ]
+	shift    #shift可跟参数，确定每次移动的位数
+done
+# 处理选项,shell会用双破折线来表明选项列表结束
+while [ -n "$1" ]
+do
+	case "$1" in
+		-a) echo "Found the -a option" ;;
+		-b) echo "Found the -b option" ;;
+		-c) echo "Found the -c option" ;;
+		--) shift
+			break ;;
+		*) echo "$1 is not an option" ;;
+	esac
+	shift
+done
+#
+count=1
+for param in $@
+do
+	echo "Parameter #$count: $param"
+	count=$[ $count + 1 ]
+done
+# 处理带值的选项
+while [ -n "$1" ]
+do
+	case "$1" in
+		-a) echo "Found the -a option";;
+		-b) param="$2"
+			echo "Found the -b option, with parameter value $param"
+			shift ;;
+		-c) echo "Found the -c option";;
+		--) shift
+			break ;;
+		*) echo "$1 is not an option";;
+	esac
+	shift
+done
+
+# 使用getopt命令，它能够识别命令行参数; 也可以使用扩展的版本:getopts
+set -- $(getopt -q ab:cd "$@")
+
+#------------------------------------------------------------------
+# 获取用户输入
+# read 命令从标准输入或文件接收输入
+echo -n "Enter your name: "
+read name
+echo "Hello $name, welcome to my program. "
+# -p 选项,允许你直接在 read 命令行指定提示符
+read -p "Please enter your age: " age
+days=$[ $age * 365 ]
+echo "That makes you over $days days old! "
+# 多个参数变量
+read -p "Enter your name: " first last
+echo "Checking data for $last, $first..."
+
+# 限时，-t 选项指定了 read 命令等待输入的秒数
+if read -t 5 -p "Please enter your name: " name
+then
+	echo "Hello $name, welcome to my script"
+else
+	echo
+	echo "Sorry, too slow! "
+fi
+# 隐藏方式读取，-s 选项可以避免在 read 命令中输入的数据出现在显示器上
+read -s -p "Enter your password: " pass
+echo
+echo "Is your password really $pass? "
+
+# 从文件中读取,每次调用 read 命令,它都会从文件中读取一行文本
+count=1
+cat test.txt | while read line
+do
+	echo "Line $count: $line"
+	count=$[ $count + 1]
+done
+echo "Finished processing the file"
+# while 循环会持续通过 read 命令处理文件中的行,直到 read 命令以非零退出状态码退出
+
 
 
